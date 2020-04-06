9f54277d09c19cfa10dca3e04429cf1d7e27f48f
lezalekss <lezalekss@gmail.com>
2020-04-06 14:01:03 +0200

    Roles and user for login

---
+++ src/main/java/com/instabot/domain/ERole.java
@@ -0,0 +1,6 @@
+package com.instabot.domain;
+
+public enum ERole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/instabot/domain/Role.java
@@ -0,0 +1,35 @@
+package com.instabot.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "role")
+public class Role {
+    @Id
+    private String id;
+
+    private ERole role;
+
+    public Role() {
+    }
+
+    public Role(ERole role) {
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ERole getRole() {
+        return role;
+    }
+
+    public void setRole(ERole role) {
+        this.role = role;
+    }
+}
